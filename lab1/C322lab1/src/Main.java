a28560645bfc4c00ac1b024f379fd8365ea28bf3
Ethan bridges <eebridge@iu.edu>
2024-01-19 11:21:23 -0500

    lab2init

--- lab1/C322lab1/src/Main.java
+++ lab1/C322lab1/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -66,4 +68,31 @@ public class Main {
         return prefix;
     }
 
+    //problem 4
+    public static int prob4(String s){
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = map.get(s.charAt(s.length() - 1));
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
 }
